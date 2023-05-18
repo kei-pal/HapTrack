840f7c394bce4a6537e0f9eac371319e2d27ca87
keipala <keipalahniuk@gmail.com>
2023-05-18 19:01:59 +0200

    add working habit graph

--- app/src/pages/habits/DetailsDialog.tsx
+++ app/src/pages/habits/DetailsDialog.tsx
@@ -1,6 +1,14 @@
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
-import React from 'react'
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import React from 'react';
 
 interface Props {
   open: boolean;
@@ -19,17 +27,66 @@ const DetailsDialog: React.FC<Props> = ({
   handleClose,
   habit,
 }) => {
-  const [id] = React.useState(habit.id);
   const [name, setName] = React.useState(habit.name);
-  const [history] = React.useState(habit.history);
+
+  // Decodes the history to an array of boolean values
+  const history = Array.from({length: 32}, (_, i) => ((habit.history >> i) & 1) === 1);
+
+  // Days of the week
+  const daysOfWeek = ['S', 'S', 'M', 'T', 'W', 'T', 'F'];
+
+  // Function to get today's day of the week
+  const getDayOfWeek = () => {
+    const date = new Date();
+    const day = date.getDay();
+    return day;
+  };
+
+  // Function to get shifted days of the week
+  const getShiftedDaysOfWeek = () => {
+    const dayOfWeek = getDayOfWeek();
+    const shiftedDaysOfWeek = [...daysOfWeek.slice(dayOfWeek), ...daysOfWeek.slice(0, dayOfWeek)];
+    return shiftedDaysOfWeek;
+  };
+
+  // Chunks the history into weeks
+  const chunks = Array(Math.ceil(history.length / 7)).fill(0).map(_ => history.splice(0, 7));
 
   return (
     <>
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>{name} Details</DialogTitle>
+      <DialogTitle>{habit.name}</DialogTitle>
+      <TextField 
+        label="Habit Name" 
+        value={habit.name}
+        sx={{m:2}}
+        InputProps={{
+          readOnly: true,
+        }}
+      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {getShiftedDaysOfWeek().map((day) => (
+                <TableCell align="center">{day}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {chunks.map((week, weekIndex) => (
+              <TableRow key={weekIndex}>
+                {week.map((value, index) => (
+                  <TableCell align="center" key={index}>{value ? "Done" : "Not Done"}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Dialog>
     </>
   )
 }
 
-export default DetailsDialog
+export default DetailsDialog;
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:32770",
+        target: "https://localhost:32768",
         changeOrigin: true,
         secure: false,
       },
